1a3a32c4523060e01c057fad916373b8ceead21c
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-05 21:33:52 +0200

    pages copied..

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-import "./App.scss";
+import './App.scss';
+import { PageWelcome } from './pages/PageWelcome';
+import { PageBooks } from './pages/PageBooks';
+import { NavLink, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App">Hallo</div>;
+	return (
+		<div className="App">
+			<h1>Info Site</h1>
+			<NavLink to="/welcome">Welcome</NavLink> |{' '}
+			<NavLink to="/books">Books</NavLink>
+			<Routes>
+				<Route path="/welcome" element={<PageWelcome />} />
+				<Route path="/books" element={<PageBooks />} />
+			</Routes>
+		</div>
+	);
 }
+
+export default App;
